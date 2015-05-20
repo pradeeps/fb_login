bed72641f0ae1c8f39c9d9e05c42b215a9584575
Pradeep Susarla <pradeep@pradeeps.org>
2015-05-20 09:19:12 +0100

    fixing inspect cmd

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,8 +1,6 @@
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
-    
-    render :text => user.inspect
     session[:user_id] = user.id
     redirect_to root_url
   end
