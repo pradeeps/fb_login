e1690e8408158873d1ba9e100c80202d02cfce70
Pradeep Susarla <pradeep@pradeeps.org>
2015-05-20 15:04:46 +0100

    fixing heroku config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -9,6 +10,7 @@ class User < ActiveRecord::Base
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
+
     end
   end
 end
